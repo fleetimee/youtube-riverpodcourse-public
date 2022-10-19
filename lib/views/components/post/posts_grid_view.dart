4b9bc03dcf49b130d4546f4f2852d802308c3fac
Vandad Nahavandipoor <vandad.np@gmail.com>
2022-10-19 05:28:35 +0200

    done with comments view

--- lib/views/components/post/posts_grid_view.dart
+++ lib/views/components/post/posts_grid_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_course/state/posts/models/post.dart';
 import 'package:instagram_clone_course/views/components/post/post_thumbnail_view.dart';
+import 'package:instagram_clone_course/views/post_comments/post_comments_view.dart';
 
 class PostsGridView extends StatelessWidget {
   final Iterable<Post> posts;
